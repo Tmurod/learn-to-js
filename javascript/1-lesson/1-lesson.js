9f236687754fd49c690eced1085994c0f985c69d
Tmurod <t.murodizmailov@gmail.com>
2023-01-25 18:45:43 +0500

    start

---
+++ javascript/1-lesson/1-lesson.js
@@ -0,0 +1,50 @@
+console.log("welcome to js");
+// data types primitive and nonprimitive
+
+
+// "string";
+// "boolean";
+// "number";
+// "symbols";
+// "null";
+// "undefined";
+// "BigInt"
+
+// "array";
+// "object";
+// "function";
+
+
+// variables
+
+
+// var
+// let
+// const
+
+// var num = 0;
+// let str = "hello";
+// const arr = [];
+
+
+// comparisions and operators
+
+// ==
+// ===
+// <
+// >
+// !
+// /
+// *
+// -
+// %
+// ++
+// --
+
+
+// methods
+
+// console.log();
+// alert();
+// confirm();
+// prompt();
---
+++ javascript/2-lesson/2-lesson.js
@@ -0,0 +1,61 @@
+// data types primitive and non-primitive
+
+// primitives
+
+// "string" : "some text";
+// "boolean" : "true" and "false";
+// "number" : 123456;
+// "symbols" : symbol(#16,p;,as);
+// "null" : "null";
+// "undefined" : "undefined";
+// "BigInt" : "BigInt(748416518515151n + 156151155n)", max_safe_integer, min_safe_integer......;
+
+// non-primitives
+
+// "array" : ["bmw", "toyota", 1243, function(), person{}];
+// "object" : {firstName: "john", age: 27, lastName: "doe"};
+// "function" : myFunction() {}, () => {}, let func = () => {};
+
+
+// variables
+
+// var
+// let
+// const
+
+// var fName = "john";
+// var lName = "doe";
+// console.log(fName + " " + lName);
+// fName = "jane";
+// console.log(fName + " " + lName);
+
+// let name = "mark";
+// let sex = "man";
+// console.log(`${name} is ${sex}`);
+// name = "manuel"
+// console.log(`${name} is ${sex}`);
+
+// const age = 27;
+// const year = 1995;
+// console.log(age + " " + year);
+// age = 28;
+// console.log(age + " " + year);
+
+// let firstName = "tmurod", lastName = "Ismoilov", borned = 1995;
+// console.log(`${firstName} ${lastName} ${borned}`);
+
+// let x;
+// x = 10;
+// console.log(x);
+
+// let x;
+// x = 10;
+// y = x;
+// let y;
+// console.log(y);
+
+// let x;
+// x = 10;
+// let y = x;
+// console.log(y);
+
