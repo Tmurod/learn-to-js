034fafe6018a175f592cbf76ccdfb843ba2792cc
Tmurod <t.murodizmailov@gmail.com>
2023-01-27 18:22:32 +0500

    3-lesson

---
+++ 3-lesson/index.js
@@ -0,0 +1,35 @@
+
+// let a = 1, b = 1;
+// bu yerda bitta variable ochilib ikkita value biriktirilyapti
+// console.log(a, b);
+// console.log(a);
+// console.log(b);
+
+// let c = ++a;
+// bu yerda "c" ni qiymati 1 lekin "++" bu belgi har doim qiymatni bittaga oshiradi shuning uchun ohirgi javob 2 bulib qoladi
+// console.log(c);
+
+// let d = b++;
+// bu yerda "d" ni qiymati "b" ga yani 1ga tenglanyapti va bittaga ortyapti qiymati lekin javobi 1 chiqadi chunki "consolda" birinchi "a" kuringanini hisobiga qiymat 1 ga oshgani bizga keyingi "consol" ni "run" qilsak kurinadi
+// console.log(d);
+
+// let a = 2;
+// let x = 1 + (a *= 2)
+// bu misolda "a" ning qiymati 2 ga teng "x" niki esa 1 ga va "x" ga "a" ni 2ga kupaytirilganini yigindisi qushilmoqda shuningdek huddi "matematika" da bulgani kabi birinchi qavs ichidagi amal bajariladi
+// console.log(x);
+
+console.log("" + 1 + 0); 
+// javobi 10 chiqadi chunki birinchi "operand" bizda "strung" data type ga mansub shuning uchun
+"" - 1 + 0;
+true + false;
+6 / "3";
+"2" * "3";
+4 + 5 + "px";
+"$" + 4 + 5;
+"4" - 2;
+"4px" - 2;
+" - 9 " + 5;
+" - 9 " - 5;
+null + 1;
+undefined + 1;
+" \n \t " - 2;
